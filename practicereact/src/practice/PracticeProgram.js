4bcfcc38b959fbc6bdfbc793dd320778f8a0e3ab
Aparna-Vivekanandan <84588336+Aparna-Vivekanandan@users.noreply.github.com>
2022-06-27 19:04:26 -0300

    New Changes

---
+++ practicereact/src/practice/PracticeProgram.js
@@ -0,0 +1,14 @@
+import "./PracticeProgram.css"
+import React, { Component } from 'react';
+
+function practiceProgram()
+{
+    console.log("hi")
+    return(
+        <div>
+            <p>Hi Sabs dear</p>
+        </div>
+    );
+}
+
+export default practiceProgram;
